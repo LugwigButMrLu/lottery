78f5542a71f491fd8177aa68b78d604a1efd21be
DaveHao <dave@fcsoft.cc>
2024-07-22 21:49:38 +0800

    workflow

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ DATA_FILES = []
 OPTIONS = {
     'argv_emulation': True,
     'packages': ['PySide6','ui','frame', 'func'],
-    'includes': ['PySide6', 'qfluentwidgets'],
+    'includes': ['qfluentwidgets'],
     'iconfile': 'icon.icns',  # 如果你有图标文件
     'plist': {
         'CFBundleName': 'Lottery',
         'CFBundleVersion': '1.1.5',
         'CFBundleIdentifier': 'com.fcsoft.lottery',
-        'LSUIElement': True,  # 如果你的应用不需要显示在 Dock 上，可以设置为 True
+        'LSUIElement': True, 
     },
 }
 
